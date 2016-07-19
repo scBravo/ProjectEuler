620bf5fdc5afefedf5da593d7d50b8ee51b6d845
scBravo <scbravo@yandex.ru>
2016-07-19 17:27:30 +0300

    Solution 8

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -11,7 +11,7 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine(LargestProduct());
         }
     }
 }
--- ProjectEuler/Solutions/Problems-1-10.cs
+++ ProjectEuler/Solutions/Problems-1-10.cs
@@ -9,6 +9,12 @@ namespace ProjectEuler.Solutions
 {
     static class Solutions1_10
     {
+        /// <summary>
+        /// Method calculate different between two parameters
+        /// </summary>
+        /// <param name="minValue">Start value</param>
+        /// <param name="maxValue">End value</param>
+        /// <returns>int32</returns>
         public static int DifferenceBetweenSqueares(int minValue, int maxValue)
         {
             int sumOfSqueares = 0;
@@ -25,20 +31,30 @@ namespace ProjectEuler.Solutions
             }
             return Math.Abs(squareOfSums - sumOfSqueares);
         }
-
-        public static int LargestProduct()
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static Int64 LargestProduct()
         {
             /*When learn some regular expression re-write this*/
             string veryBigNumber = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
-            char[] temp = veryBigNumber.ToCharArray();
-            int[] intArray = new int[temp.Length];
-            for (int i = 0; i < temp.Length; i++)
+            Int64 result = 0;
+
+            for (int i = 12; i < veryBigNumber.Length; i++)
             {
-                intArray[i] = Int32.Parse(temp[i].ToString());
+                int count = 1;
+                Int64 tempMaxProduct = Int32.Parse(veryBigNumber[i].ToString());
+                while (count <= 12)
+                {
+                    tempMaxProduct = tempMaxProduct * Int32.Parse(veryBigNumber[i - count].ToString());
+                    count++;
+                }
+                if (tempMaxProduct > result)
+                    result = tempMaxProduct;
             }
-
-
-            return 0;
+            
+            return result;
         }
     }
 }
